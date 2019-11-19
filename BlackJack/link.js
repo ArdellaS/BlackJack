ad740b6c660422514746fd6d0a26e230a0981d30
Shelton <dellashelton@rockconnections.com>
2019-11-19 11:53:45 -0500

    deal first hand set, work on other buttons

--- BlackJack/link.js
+++ BlackJack/link.js
@@ -1,9 +1,18 @@
 function Deal(min, max) {
         min = Math.ceil(1);
-        max = Math.floor(10);
-        document.getElementById("display").innerHTML = Math.floor(Math.random() * (max - min)) + min;
-        document.getElementById("display2").innerHTML = Math.floor(Math.random() * (max - min)) + min;
-}
+        max = Math.floor(11);
+        var first = document.getElementById("display").innerHTML = Math.floor(Math.random() * (max - min)) + min;
+        var second = document.getElementById("display2").innerHTML = Math.floor(Math.random() * (max - min)) + min;
+        var Sum = first + second;
+        document.getElementById("total").innerHTML = Sum;
+
+    }
+
+ /*    if (Deal().Sum > 21) {
+        document.getElementById("total").innerHTML = "Bust";
+    } else {
+        document.getElementById("total").innerHTML = Sum;
+    } */
 
 function Hit(){
     alert("awaiting function")
@@ -15,4 +24,8 @@ function Stand(){
 
 function Double(){
     alert("awaiting function")
+}
+
+if (Sum>21) {
+    alert(bust)
 }
